e04d33ab86dac5a8328ff6655457e86158fc5f9d
Lucas Gouvêa (K2) <lucas.fim@gmail.com>
2019-06-08 03:33:20 -0300

    rebuilding site sáb jun  8 03:33:20 -03 2019

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,10 @@
-$(function () {
-    console.log('test');
-});
+function submitForm() {
+  let input = {};
+  const values = $('#form-contato').serializeArray();
+
+  values.forEach(item => input[item.name] = item.value);
+
+  const subject = `${input.nome} - Contato Via Portfolio`;
+
+  window.location.href = `mailto:lucas.fim@gmail.com?subject=${subject}&body=${input.mensagem}`;
+}
